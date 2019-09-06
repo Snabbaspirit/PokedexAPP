1a873d1116cfa2ac6af07da8efe738ee76cdf89b
Ivan <snobbaspirit@gmail.com>
2019-09-06 15:10:19 +0300

    Updates

---
+++ precache-manifest.3165672989d3cb27f088f07baec55275.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6329209cb84274e0568aa9f3cfdf7d67",
+    "url": "/EPAM-Final-Project-PokedexAPP/index.html"
+  },
+  {
+    "revision": "06ef9d574b877bd36ffd",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "78abf102f7e5132c3f6a",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/css/main.139cb65e.chunk.css"
+  },
+  {
+    "revision": "06ef9d574b877bd36ffd",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/js/2.cbfb4c06.chunk.js"
+  },
+  {
+    "revision": "78abf102f7e5132c3f6a",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/js/main.4a4ed5f5.chunk.js"
+  },
+  {
+    "revision": "eab3a9076c4ba18b06cd",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/js/runtime~main.99abca1a.js"
+  },
+  {
+    "revision": "4d1ffdb462a3e65932454611b0109b2c",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/media/loading.4d1ffdb4.gif"
+  },
+  {
+    "revision": "84cd3210b8effa565706da0db99bde04",
+    "url": "/EPAM-Final-Project-PokedexAPP/static/media/pattern.84cd3210.png"
+  }
+]);
